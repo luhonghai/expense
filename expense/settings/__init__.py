67059ed616d504b20acebb606df65761a49f1e74
Jason Lu <luhonghai@gmail.com>
2017-09-28 18:29:03 +0700

    Add dist css

--- expense/settings/__init__.py
+++ expense/settings/__init__.py
@@ -3,6 +3,5 @@ from .database import *  # NOQA
 from .middlewares import *  # NOQA
 from .templates import *  # NOQA
 from .apps import *  # NOQA
-from .django import *  # NOQA
 from .logs import *
 from .mailgun import *
