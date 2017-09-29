8d97b6cf89ee5bbec972b2db02091320d4cd3f00
Jason Lu <luhonghai@gmail.com>
2017-09-29 16:11:18 +0700

    Add send debt statistic slack hook

--- expense/apps/slack_hook/views.py
+++ expense/apps/slack_hook/views.py
@@ -7,6 +7,9 @@ from rest_framework import exceptions
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 
+from apps.mobile_api.models import UserProfile
+from libs.slack_webhook import SlackWebHook
+
 
 class DebtStatistic(APIView):
     """
@@ -15,8 +18,9 @@ class DebtStatistic(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request, format=None):
-
-        return Response({
-            "username": "ExpenseBot",
-            "text": "abc"
-        })
+        debt_statistic = []
+        for user_profile in UserProfile.objects.all():
+            if user_profile.account_balance < 0:
+                debt_statistic[user_profile.name, user_profile.account_balance]
+        SlackWebHook.send_debt_notifications(debt_statistic=debt_statistic)
+        return Response({})
--- expense/libs/slack_webhook.py
+++ expense/libs/slack_webhook.py
@@ -1,12 +1,36 @@
 import requests
-slack_hook = 'https://hooks.slack.com/services/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'
+from libs.utils import format_amount
+
+expense_slack_hook = 'https://hooks.slack.com/services/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'
+debt_slack_hook = 'https://hooks.slack.com/services/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'
+
 
 class SlackWebHook(object):
 
     @classmethod
     def send_notifications(cls, text):
         data_send = {
+            "link_names": 1,
             "username": "ExpenseBot",
             "text": text
         }
-        requests.post(url=slack_hook, json=data_send)
+        requests.post(url=expense_slack_hook, json=data_send)
+
+    @classmethod
+    def send_debt_notifications(cls, debt_statistic):
+        if not debt_statistic:
+            return True
+        debt_statistic = sorted(debt_statistic, key=lambda k: k[1])
+        attachments = []
+        data_send = {
+          "link_names": 1,
+          "attachments": attachments
+        }
+
+        for debt in debt_statistic:
+            attachments.append({
+              "color": "#FF0000",
+              "text": "@%s ơi, anh còn thiếu %s VND nè! Đóng luôn đi anh. Ahihi" %(debt[0],format_amount(abs(debt[1]))),
+              "fields": []
+            })
+        requests.post(url=debt_slack_hook, json=data_send)
